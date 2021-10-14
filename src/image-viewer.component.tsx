106a720786ed705eecb1d2d7f84958c56e92139a
baiyun <baiyun.feewee.cn>
2021-10-14 13:56:13 +0800

    优化

--- src/image-viewer.component.tsx
+++ src/image-viewer.component.tsx
@@ -454,6 +454,13 @@ export default class ImageViewer extends React.Component<Props, State> {
         return <View key={index} style={{ width: screenWidth, height: screenHeight }} />;
       }
 
+      // 如果宽大于屏幕宽度,整体缩放到宽度是屏幕宽度
+      if (width > screenWidth) {
+        const widthPixel = screenWidth / width;
+        width *= widthPixel;
+        height *= widthPixel;
+      }
+
       const Wrapper = ({ children, ...others }: any) => (
         <ImageZoom
           cropWidth={this.width}
